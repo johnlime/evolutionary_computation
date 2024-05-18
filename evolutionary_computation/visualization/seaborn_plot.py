f9f152f9cc98a0b2a55f7abe14c9ef2a87b87cb3
johnlime <thelimejuiceproductions@gmail.com>
2024-05-18 16:54:29 -0400

    seaborn may not be available to everyone

--- evolutionary_computation/visualization/seaborn_plot.py
+++ evolutionary_computation/visualization/seaborn_plot.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use('seaborn')
+try:
+    plt.style.use('seaborn')
+except:
+    plt.style.use('seaborn-v0_8')
 
 import scikits.bootstrap as bootstrap
 import warnings
